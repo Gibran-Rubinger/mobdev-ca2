ed688795f8b7cc957d41e38ea72d11f8bcc378fe
Gibran Rubinger <gibranrubinger@gmail.com>
2020-04-25 10:09:04 +0000

    tabs-routing.module.ts /: create the paths

--- mobdev-ca2/src/app/app-routing.module.ts
+++ mobdev-ca2/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-
+//loadChildren technic-old route errased and loadChildren took it place
 const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)}
 ];
