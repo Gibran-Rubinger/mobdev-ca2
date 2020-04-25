cef66feff93e08c3d6ff9bb20bd1fed662b039a7
Gibran Rubinger <gibranrubinger@gmail.com>
2020-04-25 10:50:55 +0000

    api.service.ts setup import HttpClient and fuction get implemented

--- mobdev-ca2/src/app/services/api.service.ts
+++ mobdev-ca2/src/app/services/api.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+//add imports HttpClient...
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
-  constructor() { }
+//include inside the constructor what it was imported like HttpClient
+  constructor(private http: HttpClient) { }
+  //create the fuction "get" ...it need return (this.http.get(); the https request
+  //take care of the kind os commas especially with tel (id) returns... 
+    getEpisodes() {
+        return this.http.get('https://breakingbadapi.com/api/episodes');
+    }
+    getEpisode(id) {
+        return this.http.get(`https://breakingbadapi.com/api/episodes/${id}`);
+    }
+    getCharacters() {
+        return this.http.get('https://breakingbadapi.com/api/characters');
+    }
+    getCharacter(id) {
+        return this.http.get(`https://breakingbadapi.com/api/characters/${id}`);
+    }
+    getQuotes() {
+        return this.http.get('https://breakingbadapi.com/api/quotes');
+    }
+    getQuote(id) {
+        return this.http.get(`https://breakingbadapi.com/api/quotes/${id}`);
+    }
+    getDeaths() {
+        return this.http.get('https://breakingbadapi.com/api/deaths');
+    }
 }
